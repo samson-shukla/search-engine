28743b110983984d1f37ff65162756c7eccbb907
Samson Shukla <samsonshukla@gmail.com>
2021-12-29 21:16:05 +0530

    initial commit for SearchEnjin App

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/App.js",
+    "./src/index.js",
+    "./src/global.css",
+    "./src/components/Navbar.jsx",
+    "./src/components/Footer.jsx",
+    "./src/components/Routes.jsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
